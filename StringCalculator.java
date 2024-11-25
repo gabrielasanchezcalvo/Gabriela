caa7c89b681902ffa849bba65c5686eb998ab2cb
gabriela <sanchezcalvogabriela@gmail.com>
2024-11-24 23:16:09 -0300

    Implemented Add method for 0-2 numbers.

---
+++ StringCalculator.java
@@ -0,0 +1,23 @@
+package Gabriela;
+public class StringCalculator {
+    public static void main(String[] args) {
+        StringCalculator calculator = new StringCalculator();
+        System.out.println(calculator.add("")); 
+        System.out.println(calculator.add("1")); 
+        System.out.println(calculator.add("1.5,30.5")); 
+    }
+    public int add(String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] parts = numbers.split(",");
+        int sum = 0;
+        for (String part : parts) {
+            sum += Double.parseDouble(part.trim());
+        }
+        return sum;
+    }
+    // nombre del commit: Implemented Add method for 0-2 numbers.
+
+    
---
+++ tringCalculator.fs
@@ -0,0 +1,15 @@
+module StringCalculator
+
+let add (numbers: string) =
+    match numbers with
+    | "" -> 0
+    | _ ->
+        let parts = numbers.Split(',')
+        parts |> Array.map int |> Array.sum
+
+// Pruebas
+printfn "%d" (add "")       // 0
+printfn "%d" (add "1")      // 1
+printfn "%d" (add "1,2")    // 3
+
+//Commit: Implemented Add method for 0-2 numbers.
