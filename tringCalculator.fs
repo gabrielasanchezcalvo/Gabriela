153ac43aaff065596e5b735e14252d12b159c023
gabriela <sanchezcalvogabriela@gmail.com>
2024-11-25 00:31:26 -0300

    Added support for delimiters of any length.

--- tringCalculator.fs
+++ tringCalculator.fs
@@ -3,14 +3,21 @@ module StringCalculator
 let add (numbers: string) =
     match numbers with
     | "" -> 0
+    | _ when numbers.StartsWith("//") ->
+        let delimiterEnd = numbers.IndexOf('\n')
+        let customDelimiter = numbers.[2..delimiterEnd-1]
+        let numbersPart = numbers.[delimiterEnd+1..]
+        let parts = numbersPart.Split([| customDelimiter |], System.StringSplitOptions.None)
+        parts |> Array.map int |> Array.filter (fun x -> x <= 1000) |> Array.sum
     | _ ->
         let delimiters = [| ','; '\n' |]
-        let parts = numbers.Split(delimiters) |> Array.map int
-        parts |> Array.filter (fun x -> x <= 1000) |> Array.sum
+        let parts = numbers.Split(delimiters)
+        parts |> Array.map int |> Array.filter (fun x -> x <= 1000) |> Array.sum
 
 // Pruebas
-printfn "%d" (add "2,1001")   // 2
-printfn "%d" (add "1000,1")   // 1001
+printfn "%d" (add "//[***]\n1***2***3") // 6
+
+
 
 
 
